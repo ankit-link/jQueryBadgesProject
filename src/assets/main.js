0e87fe8faac92821de738c96a22bac4cd64012e6
Ankit Pandey <ankit.pandey@yapsody.com>
2016-12-02 20:33:13 +0530

    Added completed course

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,24 @@
 $(function() {
 
-  // your code will go here
+   $.ajax({
+    url: 'https://www.codeschool.com/users/ankit123.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+      var completed = response.courses.completed;
+      var badges = $('#badges');
+      $.each(completed,function(ind,val){
+      	var title = $('<h3>'+ val.title + '</h3>')
+      	var html = $('<div class="course"/>')
+      	var img = $('<img src="'+ val.badge+'">')
+      	var anchor = $('<a target="_blank" class="btn btn-primary" href="' + val.url + ' ">See course </a>' )
+      	html.append(title);
+      	html.append(img);
+      	html.append(anchor);
+      	badges.append(html);
+      })
+      console.log("response received",response)
+    }
+  });
 
 });
